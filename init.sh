3ce4282beaedecdcdc7c78debd94ce5600c64878
Maksim Murygin <muriginm@gmail.com>
2017-09-15 07:41:22 +0700

    update init script

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-gunicorn -c "$(pwd)/etc/hello.py" --chdir "$(pwd)/web" hello:app &
-sudo ln -sf "$(pwd)/web/etc/nginx.conf"  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+sudo ln -sf "$(pwd)/etc/nginx.conf"  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx reload
+
+sudo ln -sf "$(pwd)/etc/hello.py" /etc/gunicorn.d/hello.py
+gunicorn -c /etc/gunicorn.d/hello.py --chdir "$(pwd)/web" hello:app &
