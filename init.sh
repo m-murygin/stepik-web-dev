311e16201d3fdfee41af559130b4a0321a9ecc4d
Maksim Murygin <muriginm@gmail.com>
2017-09-22 09:51:29 +0700

    correct run configs

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo ln -sf "$(pwd)/etc/nginx.conf"  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+sudo /etc/init.d/nginx reload
 
-sudo ln -sf "$(pwd)/etc/hello.py" /etc/gunicorn.d/hello.py
-gunicorn -c /etc/gunicorn.d/hello.py --chdir "$(pwd)/web" hello:app &
+# gunicorn -c "$(pwd)/etc/hello.py" hello:app
+gunicorn -c "$(pwd)/etc/ask.py" --chdir "$(pwd)/ask" ask.wsgi
