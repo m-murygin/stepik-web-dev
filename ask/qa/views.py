8e39776a55915845fb9b28cfb0f803fc575cd130
Maksim Murygin <muriginm@gmail.com>
2017-11-18 15:09:29 +0700

    add base layout

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.views.decorators.http import require_GET
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
@@ -10,7 +11,7 @@ from models import Question
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
-
+@require_GET
 def question_view(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
@@ -19,9 +20,11 @@ def question_view(request, question_id):
     })
 
 
+@require_GET
 def question_list(request, *args, **kwargs):
     return HttpResponse('OK_list')
 
 
+@require_GET
 def popular(request, *args, **kwargs):
     return HttpResponse('OK_popular')
