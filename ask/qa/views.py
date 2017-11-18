5a7218d4b5ccb12d705213bebef69e161c88e921
Maksim Murygin <muriginm@gmail.com>
2017-11-18 14:26:10 +0700

    configure urls

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+
+def question(request, *args, **kwargs):
+    return HttpResponse('OK_question')
+
+
+def question_list(request, *args, **kwargs):
+    return HttpResponse('OK_list')
+
+
+def popular(request, *args, **kwargs):
+    return HttpResponse('OK_popular')
